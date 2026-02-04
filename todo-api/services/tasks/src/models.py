f621e18cc7e1fd7462e6c346d44cdc252dc4c5f4
calabria1 <arthurcalabriaa@gmail.com>
2026-02-04 00:28:20 -0300

    .

--- todo-api/services/tasks/src/models.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-class TaskStatus:
-    PENDENTE = "Pendente"
-    EM_ANDAMENTO = "Em Andamento"
-    CONCLUIDA = "Concluída"
-
-ALLOWED_STATUS = {TaskStatus.PENDENTE, TaskStatus.EM_ANDAMENTO, TaskStatus.CONCLUIDA}
